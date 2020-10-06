1f59a2fb51797c3d28e290ce8456d86c9db8e4b5
korks2931 <71141021+korks2931@users.noreply.github.com>
2020-10-06 14:45:11 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,38 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program will find the factorial of the entered integer.
+ * @author Sevde
  */
 public class Main {
 
-  /**
+  /** 
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a Scanner for user input 
+    Scanner input = new Scanner(System.in);
+    // ask the user to enter an integer
+    System.out.println("Please enter an integer to calculate the factorial of");
+    int integerUser = input.nextInt();
+    // the accumulator
+    int total = 0;
+    // the factorial of the number 
+    int factorial = 1;
+    // for loop to repeat
+    for(int count = 1;  count <= integerUser; count= count + 1  ){
+      // add the number as we go into 
+      total = total + count;
+      // find the factorial of the integer
+      factorial *= count;
+      
+      
+      }
+      System.out.println( integerUser + "!= " + factorial );
+
+
+
   }
+    
 }
+
